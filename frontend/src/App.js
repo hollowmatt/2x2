9b98d5ea747a21f5ecfde1cd28836c9dd7e6bb8e
Matt Holloway <hollowmatt@gmail.com>
2023-05-14 21:21:38 -0400

    updates for GCP, but need to fix Chakra issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
 import './App.css';
-import AppRouter from './router/AppRouter';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import Register from './components/Register';
+import Login from './components/Login';
+import Dashboard from './components/Dashboard';
+import Create from './components/Create';
+
 
 function App() {
   return (
     <div>
-      <AppRouter />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/register' element={<Register/>} />
+          <Route path='/dashboard' element={<Dashboard />} />
+          <Route path='/create' element={<Create />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

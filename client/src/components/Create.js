9724e5860a7835654a70396f0752567ba24f9d0f
hollowmatt <hollowmatt@gmail.com>
2023-05-24 14:33:06 -0400

    start work on 'create' page

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -1,6 +1,33 @@
+import { Card, Stack, Image, CardBody, Heading, Flex, Button } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import Nav from "./Nav";
+const BASE_PATH = process.env.REACT_APP_API_ADDRESS;
+
 function Create() {
+  const [wbr, setWbr] = useState({});
+  const [wbrTypes, setWbrTypes] = useState([]);
+
+  useEffect(() => {
+    const fetchWbrTypes = () => {
+      fetch(BASE_PATH + "/api/all/wbrtypes", {
+      })
+        .then((res) => res.json())
+        .then((data) => setWbrTypes(data.wbrtypes))
+        .catch((err) => console.error(err));
+    };
+    fetchWbrTypes();
+  },[]);
+
   return(
-    <p>Placeholder</p>
+    <div>
+      <Nav/>
+      <p>Placeholder</p>
+      <select id='wbrType' name='wbrType' onChange={(e) => setWbr(e.target.value)}>
+        {wbrTypes.map((wbrtype) => (
+          <option value={wbrtype.id} key={wbrtype.id}>{wbrtype.type}</option>
+        ))}
+      </select>
+    </div>
   );
 }
 
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,4 +1,4 @@
-import { Card, Stack, Image, CardBody, Heading, Flex, Button } from "@chakra-ui/react";
+import { Card, Stack, Image, CardBody, Heading, Flex, Button, Spinner } from "@chakra-ui/react";
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from 'react-router-dom';
 
